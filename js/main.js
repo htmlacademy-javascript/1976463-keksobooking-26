6c05abc46bc2094653aaa946c2484bac12fc6fc6
Robert <leifman.robert@gmail.com>
2022-06-13 09:33:16 +0300

    deleted console.log

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@ import {createAdvert} from './data.js';
 const ADVERTS_NUMBER = 10;
 
 const createMultipleAdverts = (count) => Array.from({length: count}, (x, i) => createAdvert(i));
-console.log(createMultipleAdverts(ADVERTS_NUMBER));
+createMultipleAdverts(ADVERTS_NUMBER);
