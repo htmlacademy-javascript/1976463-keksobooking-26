f3968fe2d48fa0fa7f4327aea324e1dd52cb618e
Robert <leifman.robert@gmail.com>
2022-07-04 12:59:58 +0300

    task 9.9

--- js/form.js
+++ js/form.js
@@ -14,7 +14,6 @@ const disableForm = function () {
     fieldset.disabled = true;
   });
 };
-
 disableForm();
 
 const activateForm = function () {
--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
-import {createMultipleAdverts} from './data.js';
-import {generateAdvertElements} from './generator.js';
-import {activateForm} from './form.js';
 import './form.js';
+import {activateMap} from './map.js';
+activateMap();
 
-activateForm();
-const ADVERTS_NUMBER = 1;
-generateAdvertElements(createMultipleAdverts(ADVERTS_NUMBER));
